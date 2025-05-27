626896382414cdf3426dc59a8e43ca3590fd9054
Haiko <haiko.erinkveld@gmail.com>
2025-05-27 21:17:42 +0200

    created navbar + hero + layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <AppNavBar />
+    <main class="container mx-auto flex-grow">
+      <slot />
+    </main>
+    <footer class="bg-gray-800 text-white p-4 text-center">
+      © 2025 Travel Log. All rights reserved.
+    </footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero bg-base-200 min-h-96">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Hello there
+        </h1>
+        <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+          quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <button class="btn btn-accent">
+          Login with github
+          <Icon
+            name="tabler:brand-github"
+            class="ml-1"
+            size="24"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
