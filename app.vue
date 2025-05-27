c2918ece6d71cd5f5f171a14ed4b41ab09c68292
Haiko-E <80853010+Haiko-E@users.noreply.github.com>
2025-05-27 21:20:22 +0200

    created navbar + hero + layout (#50)

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-red-500">
-      works
-    </h1>
-    <button class="btn">
-      test <Icon name="tabler:map" size="32" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ components/app/nav-bar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost normal-case text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Login with github
+        <Icon
+          name="tabler:brand-github"
+          class="ml-1"
+          size="24"
+        />
+      </button>
+    </div>
+  </div>
+</template>
