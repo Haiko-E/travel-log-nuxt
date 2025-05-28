a4c78e2ec6c6fa566b91974cd1e576640ea63281
Haiko <haiko.erinkveld@gmail.com>
2025-05-28 19:33:21 +0200

    implement better auth

--- lib/env.ts
+++ lib/env.ts
@@ -4,6 +4,8 @@ export const envSchema = z.object({
   NODE_ENV: z.string(),
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string().optional(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof envSchema>;
--- server/database/index.ts
+++ server/database/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/libsql";
 
-import env from "~/lib/env";
-
+import env from "../../lib/env";
 import * as schema from "./schema/schema";
 
 const db = drizzle({
