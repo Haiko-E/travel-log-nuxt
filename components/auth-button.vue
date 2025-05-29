70b50360dd48a8a5286cb91c7ddcf511a78e16c5
Haiko <haiko.erinkveld@gmail.com>
2025-05-29 19:24:54 +0200

    add logout + redirects

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -1,33 +1,42 @@
-<script setup lang="ts">
-import { authClient } from "~/lib/auth-client";
-
-const session = authClient.useSession();
-
-async function signIn() {
-  await authClient.signIn.social({
-    provider: "github",
-  });
-}
+<script lang="ts" setup>
+const authStore = useAuthStore();
 </script>
 
 <template>
+  <div v-if="!authStore.isLoading && authStore.user" class="dropdown dropdown-end">
+    <div
+      tabindex="0"
+      role="button"
+      class="btn m-1"
+    >
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
   <button
-    v-if="!session.data"
+    v-else
+    :disabled="authStore.isLoading"
     class="btn btn-accent"
-    @click="signIn"
+    @click="authStore.signIn"
   >
-    Login with github
+    Sign In With Github
+    <span v-if="authStore.isLoading" class="loading loading-spinner loading-md" />
     <Icon
+      v-else
       name="tabler:brand-github"
-      class="ml-1"
       size="24"
     />
-  </button>
-  <button
-    v-else-if="session.data"
-    class="btn btn-accent"
-    @click="authClient.signOut()"
-  >
-    Sign out
   </button>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
   <div class="flex flex-col min-h-screen">
     <AppNavBar />
